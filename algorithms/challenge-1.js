72e25fb64b903e066543453fca32458eefd31149
Nnadozie Opara <nnadozieopara@yahoo.com>
2022-03-04 06:32:58 +0100

    Challenge 1 and 3 updated

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,28 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+    let initial = [0,0,0,0,0,0,0,0,0];
+    let result =[];
+    let length = numArr.length;
+    for(let i=0; i<length; i++){
+        initial[numArr[i] -1] = numArr[i]
+    }
+
+    for(let i=0; i<9; i++){
+        if(initial[i] === 0){
+            result.push(i+1);
+        }
+    }
+    
+    if(result.length === 0){
+        return "false"
+    }else if(result.length === 1){
+        return result[0];
+    }else{
+        return result;
+    }
+}
+
+console.log(missingNumber([1,2,3,4,6,7,8,9]));
+console.log(missingNumber([1,2,3,4,5,6,8]));
+console.log(missingNumber([1,2,3,4,5,6,7,8,9]));
 
 module.exports = missingNumber
--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,13 @@
-function increment(arr, value) {}
+function increment(arr, value) {
+    let length = arr.length;
+    for(let i=0; i<length; i++){
+        
+        arr[i]["val"] += value;
+    }
+    return arr;
+}
+
+console.log(increment([{ val: 1 }, { val: 2}, { val: 3 }], 2));
+console.log(increment([{ val: 10 }, { val: 20}, { val: 30 }], 5));
 
 module.exports = increment
